8d82061fc0f0da1bfea2bb21d21a84ba472866cb
Mike Bowen <mike.scott.bowen@gmail.com>
2015-10-04 12:40:02 -0500

    initial

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appApp
+ * @description
+ * # appApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appApp', [
+    'ngMessages',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl',
+        controllerAs: 'home'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appApp.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the appApp
+ */
+angular.module('appApp')
+  .controller('HomeCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
