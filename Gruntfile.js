b45d9700437bc3e9df53bbd73dec6c4fe53f22df
mbowen000 <mike@smbhd.com>
2016-01-08 16:20:19 -0600

    changing port to support heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -97,7 +97,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9001,
+        port: process.env.PORT || 9001,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
         livereload: 35730
