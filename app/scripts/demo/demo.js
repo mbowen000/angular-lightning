b29755fb3e25e0d1b823cb6df5e6ae513ae9c289
Ashar <ashar@smbhd.com>
2016-01-08 16:54:26 -0600

    add danger alert type

--- app/scripts/demo/demo.js
+++ app/scripts/demo/demo.js
@@ -21,8 +21,17 @@ angular.module('angular-lightning-demo', ['angular-lightning', 'angular-lightnin
 		      return limitToFilter(response.data.results, 15);
 		    });
 		},
-		sendInform: function(val) {
-			inform.add(val);
+		sendInform: function(val, type) {
+			if (!val) {
+				val = 'Demo';
+			}
+
+			if (type === 'danger') {
+				inform.add(val, {type: 'danger'});
+			}
+			else {
+				inform.add(val);
+			}
 		}
 	});
 }]);
