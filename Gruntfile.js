07ac4f55b44a6104b350cef7ac143b4e41f0a81a
mbowen000 <mike@smbhd.com>
2016-01-08 16:24:46 -0600

    changing bind address

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,7 +99,7 @@ module.exports = function (grunt) {
       options: {
         port: process.env.PORT || 9001,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35730
       },
       livereload: {
